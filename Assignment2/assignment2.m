b6e63a1dafbc9e39c29bae319f8c733285b929e1
meltonc11 <meltonc11@students.ecu.edu>
2016-09-01 11:21:18 -0400

    Initial solution

---
+++ Assignment2/assignment2.m
@@ -0,0 +1,65 @@
+% Caitlin Melton
+% Due Date: 09/15/16
+% KINE 6803: Assignment 2
+
+%% Chapter 1 Exercises
+% 1.)CuAW = 63.55
+% 2.)myage = 23
+%    myage - 2
+%    myage + 1
+% 3.)CharacterMax = namelengthmax
+% 4.)ounces = 72
+%    pounds = 4.5
+%    who
+%    whos
+%    clear ounces
+%    who 
+%    whos
+% 5.)Min32 = intmin('uint32')
+%    Max32 = intmax('uint32')
+%    Min64 = intmin('uint64')
+%    Max64 = intmax('uint64')
+% 6.)variablechange = 4.5
+%    variablechanged = int32(variablechange)
+% 11.)lbs = 135
+%     kilos = lbs/2.2
+% 12.)ftemp = 32
+%     ctemp = (ftemp - 32)*(5/9)
+% 13.)inches = 36
+%     feet = inches/12
+% 14.)sind(90)
+%     sin(1.57)
+% 15.)R1=100
+%     R2=200
+%     R3=300
+%     RT=R1+R2+R3
+% 22.)lowercase letters come before upercase letters
+% 24.)1,1,1,0 so false
+% 25.)x=6
+%     y=22
+%     xor(x>5,y<10)
+% 26.)3*(10^5)==3e5
+% 27.)log10(10000)==4
+
+%% Chapter 2 Exercises
+% 1.)
+% 2.)
+% 3.)
+% 4.)
+% 6.)
+% 7.)
+% 8.)
+% 9.)
+% 10.)
+% 12.)
+% 23.)
+% 26.)
+% 30.)
+% 31.)
+
+%% Chapter 3 Exercises
+% 1.)
+% 4.)
+% 6.)
+% 8.)
+% 13.)
