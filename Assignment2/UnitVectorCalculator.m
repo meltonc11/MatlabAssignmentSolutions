88342c091c1382a1b80d526324d895c3e3313fc3
meltonc11 <meltonc11@students.ecu.edu>
2016-09-15 13:17:13 -0400

    Delete UnitVectorCalculator.m

--- Assignment2/UnitVectorCalculator.m
+++
@@ -1,9 +0,0 @@
-%%This script calculates the unit vector associated with an instantaneous
-%%velocity in the x,y,and z directions
-%It prompts the user for the x,y,and z direction of the particle position
-%Prompt user for each of the directions of the particle position
-x = input('Enter in the velocity in the x direction:');
-y = input('Enter in the velocity in the y direction:');
-z = input('Enter in the velocity in the z direction:');
-%Calculates the unit vector based on those directions
-uvec = [x y z]/(sqrt((x^2)+(y^2)+(z^2)))
