2e36eab01e4eb0b8c9b7b2c320b020c2b38d243d
meltonc11 <meltonc11@students.ecu.edu>
2016-09-22 13:10:21 -0400

    Almost done
    Finishing touches needed

--- Assignment3/TicTacToe.m
+++ Assignment3/TicTacToe.m
@@ -2,7 +2,7 @@
 %Caitlin Melton
 %9/13/16 
 %Due Date: 9/27/16
-%This script enables the user to play a computer in a Tic Tac Toe match and
+%This script enables thex user to play a computer in a Tic Tac Toe match and
 %will have the computer moves generated automatically
 
 %%  Welcome Message for the User
@@ -11,7 +11,7 @@ disp('Ready or Not, Here We Go!');
 Board = ['-' '-' '-'; '-' '-' '-'; '-' '-' '-'];
 disp(Board);
 %%  Player Chooses Which Character (either X or O)
-user = input('Do you want to be X or O:\n>','s');
+user = input('Do you want to be X or O? (Please enter your answer with uppercase letters):\n>','s');
 if user == 'X';
     comp = 'O';
 else
@@ -42,10 +42,39 @@ else
    spotuser1 = inputdlg(prompt1,response1);
    cuser1 = str2double(spotuser1{2});
    ruser1 = str2double(spotuser1{1});
+   if Board(ruser1,cuser1) == '-';
    Board(ruser1,cuser1) = user;
+   else
+   disp('Input new spot choice');
+   prompt1 = {'ROW (1, 2, or 3)', 'COLUMN (1, 2, or 3)'};
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser1 = inputdlg(prompt1,response1);
+   cuser1 = str2double(spotuser1{2});
+   ruser1 = str2double(spotuser1{1});
+    if Board(ruser1,cuser1) == '-';
+   Board(ruser1,cuser1) = user;
+    else
+   disp('Input new spot choice');
+   prompt1 = {'ROW (1, 2, or 3)', 'COLUMN (1, 2, or 3)'};
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser1 = inputdlg(prompt1,response1);
+   cuser1 = str2double(spotuser1{2});
+   ruser1 = str2double(spotuser1{1});
+    if Board(ruser1,cuser1) == '-';
+   Board(ruser1,cuser1) = user;
+    else
+   disp('Input new spot choice');
+   prompt1 = {'ROW (1, 2, or 3)', 'COLUMN (1, 2, or 3)'};
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser1 = inputdlg(prompt1,response1);
+   cuser1 = str2double(spotuser1{2});
+   ruser1 = str2double(spotuser1{1});
+   Board(ruser1,cuser1) = user;
+    end
+    end
+   end
 end
 disp(Board)
-%%  Keep Going Until There is a Winner or no More Available Moves
 %% Computer completes its second turn
 disp('Computer completes its second turn.')
 if Board(1,1) == comp && Board(2,2) == '-';
@@ -159,10 +188,73 @@ else
    spotuser2 = inputdlg(prompt1,response1);
    cuser2 = str2double(spotuser2{2});
    ruser2 = str2double(spotuser2{1});
+   if Board(ruser2,cuser2) == '-';
    Board(ruser2,cuser2) = user;
+   else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+        if Board(ruser2,cuser2) == '-';
+   Board(ruser2,cuser2) = user;
+        else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+            if Board(ruser2,cuser2) == '-';
+   Board(ruser2,cuser2) = user;
+            else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+                if Board(ruser2,cuser2) == '-';
+   Board(ruser2,cuser2) = user;
+                else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+                    if Board(ruser2,cuser2) == '-';
+   Board(ruser2,cuser2) = user;
+                    else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+                        if Board(ruser2,cuser2) == '-';
+   Board(ruser2,cuser2) = user;
+                        else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+                            if Board(ruser2,cuser2) == '-';
+   Board(ruser2,cuser2) = user;
+                            else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser2 = inputdlg(prompt1,response1);
+   cuser2 = str2double(spotuser2{2});
+   ruser2 = str2double(spotuser2{1});
+   Board(ruser2,cuser2) = user;
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
 end
 disp(Board)
-%% Computer completes its fourth turn by trying to either put three in a row or block the user from doing so
+%% Computer completes its third turn by trying to either put three in a row or block the user from doing so
 disp('Computer completes its third turn')
 if Board(1,1) == comp && Board(1,2) == comp && Board(1,3) == '-';
     Board(1,3) = comp; % starts trying to connect its own
@@ -262,6 +354,23 @@ elseif Board(3,2) == user && Board(3,3) == user && Board(3,1) == '-';
     Board(3,1) = comp;
 end
 disp(Board)
+if Board(1,1) == comp && Board(1,2) == comp && Board(1,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,1) == comp && Board(2,1) == comp && Board(3,1) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,1) == comp && Board(2,2) == comp && Board(3,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,2) == comp && Board(2,2) == comp && Board(3,2) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,3) == comp && Board(2,2) == comp && Board(3,1) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,3) == comp && Board(2,3) == comp && Board(3,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(2,1) == comp && Board(2,2) == comp && Board(2,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(3,1) == comp && Board(3,2) == comp && Board(3,3) == comp;
+    disp('Game Over. Computer Wins!')
+else 
 %% User is prompted for their third turn
 disp('Please input your third spot choice into the pop up dialogue box.')
 spotuser3 = inputdlg(prompt1,response1);
@@ -275,9 +384,98 @@ else
    spotuser3 = inputdlg(prompt1,response1);
    cuser3 = str2double(spotuser3{2});
    ruser3 = str2double(spotuser3{1});
+   if Board(ruser3,cuser3) == '-';
    Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+  if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+   if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+   if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+   if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+  if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser1 = str2double(spotuser3{2});
+   ruser1 = str2double(spotuser3{1});
+   if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+   if Board(ruser3,cuser3) == '-';
+   Board(ruser3,cuser3) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser3 = inputdlg(prompt1,response1);
+   cuser3 = str2double(spotuser3{2});
+   ruser3 = str2double(spotuser3{1});
+   Board(ruser3,cuser3) = user;
+   end
+   end
+   end
+   end
+   end
+   end
+   end
+   end
 end
 disp(Board)
+if Board(1,1) == user && Board(1,2) == user && Board(1,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,1) == user && Board(2,1) == user && Board(3,1) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,1) == user && Board(2,2) == user && Board(3,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,2) == user && Board(2,2) == user && Board(3,2) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,3) == user && Board(2,2) == user && Board(3,1) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,3) == user && Board(2,3) == user && Board(3,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(2,1) == user && Board(2,2) == user && Board(2,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(3,1) == user && Board(3,2) == user && Board(3,3) == user;
+    disp('Game Over. You Win!')
+else
 %% Computer completes its fourth turn by trying to either put three in a row or block the user from doing so
 disp('Computer completes its fourth turn')
 if Board(1,1) == comp && Board(1,2) == comp && Board(1,3) == '-';
@@ -396,6 +594,23 @@ elseif Board(3,3) == '-';
     Board(3,3) = comp;
 end
 disp(Board)
+if Board(1,1) == comp && Board(1,2) == comp && Board(1,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,1) == comp && Board(2,1) == comp && Board(3,1) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,1) == comp && Board(2,2) == comp && Board(3,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,2) == comp && Board(2,2) == comp && Board(3,2) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,3) == comp && Board(2,2) == comp && Board(3,1) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(1,3) == comp && Board(2,3) == comp && Board(3,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(2,1) == comp && Board(2,2) == comp && Board(2,3) == comp;
+    disp('Game Over. Computer Wins!')
+elseif Board(3,1) == comp && Board(3,2) == comp && Board(3,3) == comp;
+    disp('Game Over. Computer Wins!')
+else
 %% User is prompted for their fourth turn
 disp('Please input your fourth and final spot choice into the pop up dialogue box.')
 spotuser4 = inputdlg(prompt1,response1);
@@ -409,9 +624,107 @@ else
    spotuser4 = inputdlg(prompt1,response1);
    cuser4 = str2double(spotuser4{2});
    ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
    Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+ if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   if Board(ruser4,cuser4) == '-';
+   Board(ruser4,cuser4) = user;
+else
+   disp('Input new spot choice');
+   disp('Error: You have entered a previously used spot. Please enter a different choice of row and column.');
+   spotuser4 = inputdlg(prompt1,response1);
+   cuser4 = str2double(spotuser4{2});
+   ruser4 = str2double(spotuser4{1});
+   Board(ruser4,cuser4) = user;
+   end
+   end
+   end
+   end
+  end
+   end
+   end
+   end
+    end
 end
 disp(Board)
+if Board(1,1) == user && Board(1,2) == user && Board(1,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,1) == user && Board(2,1) == user && Board(3,1) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,1) == user && Board(2,2) == user && Board(3,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,2) == user && Board(2,2) == user && Board(3,2) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,3) == user && Board(2,2) == user && Board(3,1) == user;
+    disp('Game Over. You Win!')
+elseif Board(1,3) == user && Board(2,3) == user && Board(3,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(2,1) == user && Board(2,2) == user && Board(2,3) == user;
+    disp('Game Over. You Win!')
+elseif Board(3,1) == user && Board(3,2) == user && Board(3,3) == user;
+    disp('Game Over. You Win!')
+else
 %% Computer completes its fifth turn by trying to either put three in a row or block the user from doing so
 disp('Computer completes its fifth turn')
 if Board(1,1) == comp && Board(1,2) == comp && Board(1,3) == '-';
@@ -553,4 +866,8 @@ elseif Board(3,3) == '-';
 end
 if freecells == 0;
     disp('Game Over. It is a Draw!');
+end
+end
+end
+end
 end
